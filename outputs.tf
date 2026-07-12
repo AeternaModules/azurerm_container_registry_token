dd4ada0d8ecf2c54390662a5c787487769127c0b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "container_registry_tokens" {
-  description = "All container_registry_token resources"
-  value       = azurerm_container_registry_token.container_registry_tokens
-}
 output "container_registry_tokens_container_registry_name" {
-  description = "List of container_registry_name values across all container_registry_tokens"
-  value       = [for k, v in azurerm_container_registry_token.container_registry_tokens : v.container_registry_name]
+  description = "Map of container_registry_name values across all container_registry_tokens, keyed the same as var.container_registry_tokens"
+  value       = { for k, v in azurerm_container_registry_token.container_registry_tokens : k => v.container_registry_name }
 }
 output "container_registry_tokens_enabled" {
-  description = "List of enabled values across all container_registry_tokens"
-  value       = [for k, v in azurerm_container_registry_token.container_registry_tokens : v.enabled]
+  description = "Map of enabled values across all container_registry_tokens, keyed the same as var.container_registry_tokens"
+  value       = { for k, v in azurerm_container_registry_token.container_registry_tokens : k => v.enabled }
 }
 output "container_registry_tokens_name" {
-  description = "List of name values across all container_registry_tokens"
-  value       = [for k, v in azurerm_container_registry_token.container_registry_tokens : v.name]
+  description = "Map of name values across all container_registry_tokens, keyed the same as var.container_registry_tokens"
+  value       = { for k, v in azurerm_container_registry_token.container_registry_tokens : k => v.name }
 }
 output "container_registry_tokens_resource_group_name" {
-  description = "List of resource_group_name values across all container_registry_tokens"
-  value       = [for k, v in azurerm_container_registry_token.container_registry_tokens : v.resource_group_name]
+  description = "Map of resource_group_name values across all container_registry_tokens, keyed the same as var.container_registry_tokens"
+  value       = { for k, v in azurerm_container_registry_token.container_registry_tokens : k => v.resource_group_name }
 }
 output "container_registry_tokens_scope_map_id" {
-  description = "List of scope_map_id values across all container_registry_tokens"
-  value       = [for k, v in azurerm_container_registry_token.container_registry_tokens : v.scope_map_id]
+  description = "Map of scope_map_id values across all container_registry_tokens, keyed the same as var.container_registry_tokens"
+  value       = { for k, v in azurerm_container_registry_token.container_registry_tokens : k => v.scope_map_id }
 }
 
